9e06dd97703f9ff098481865f1288ca01ba1e2f4
Vladislav Kohai <cherry.designwork@gmail.com>
2023-02-25 21:24:13 +0300

    practice_003 is completed

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many movies have you watched?");
+let numberOfFilms;
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    private: false
 };
 
-for (let index = 0; index < 2;) {
-    const lastWatched = prompt("What was the last movie you watched?"),
-        ratingOfMovie = prompt("How do you rate it?");
-    if (lastWatched === null ||
-        lastWatched.length > 50 ||
-        lastWatched == '' ||
-        ratingOfMovie == null ||
-        ratingOfMovie == '') {
-        continue;
+function start() {
+    numberOfFilms = +prompt("How many movies have you watched?");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many movies have you watched?");
     }
-    personalMovieDB.movies[lastWatched] = ratingOfMovie;
-    index++
 }
 
-if (personalMovieDB.count < 10) {
-    alert("You watched a few movies");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert("You are a good watcher");
-} else {
-    alert("You are a movie man");
+function rememberMyFilms() {
+    for (let index = 0; index < 2;) {
+        const lastWatched = prompt("What was the last movie you watched?"),
+            ratingOfMovie = prompt("How do you rate it?");
+        if (lastWatched === null ||
+            lastWatched.length > 50 ||
+            lastWatched == '' ||
+            ratingOfMovie == null ||
+            ratingOfMovie == '') {
+            continue;
+        }
+        personalMovieDB.movies[lastWatched] = ratingOfMovie;
+        index++
+    }
 }
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("You watched a few movies");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert("You are a good watcher");
+    } else {
+        alert("You are a movie man");
+    }
+}
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; ++i) {
+        personalMovieDB.genres[i] = prompt(`Your favourite genre under the number ${i + 1}`);
+    }
+}
+
+start();
+rememberMyFilms();
+detectPersonalLevel();
+showMyDB(personalMovieDB.private);
