95abadcd91533193327a3414d332d828459e94d9
Vladislav Kohai <cherry.designwork@gmail.com>
2023-02-25 19:51:31 +0300

    Practice__02 completed

--- script.js
+++ script.js
@@ -10,9 +10,22 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let index = 0; index < 2; index++) {
-    const lastWatched = prompt("What was the last movie you watched?"),
-        ratingOfMovie = prompt("How do you rate it?");
+for (let index = 0; index < 2;) {
+    const lastWatched = prompt("What was the last movie you watched?");
+    if (lastWatched === undefined || lastWatched.length > 50) {
+        continue;
+    }
+    const ratingOfMovie = prompt("How do you rate it?");
     personalMovieDB.movies[lastWatched] = ratingOfMovie;
+    index++
 }
+
+if (personalMovieDB.count < 10) {
+    alert("You watched a few movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("You are a good watcher");
+} else {
+    alert("You are a movie man");
+}
+
 console.log(personalMovieDB);
