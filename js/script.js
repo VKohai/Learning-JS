3d224f4814f30fa8dbba2ba1c0d0dd64c20932f0
Kohaikaa <cherry.designwork@gmail.com>
2023-05-02 20:00:55 +0300

    Calculator part 2 is finished

--- js/script.js
+++ js/script.js
@@ -370,12 +370,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Calculator
   const calories = document.querySelector('.calculating__result span');
-  let sex = document.querySelector('#gender .calculating__choose-item_active').getAttribute('id'),
-    ratio = +document.querySelector('.calculating__choose_big .calculating__choose-item_active').getAttribute('data-ratio'),
-    height, weight, age;
+  let sex, ratio, height, weight, age;
+
+  if (localStorage.getItem('sex'))
+    sex = localStorage.getItem('sex');
+  else
+    sex = document.querySelector('#gender .calculating__choose-item_active').getAttribute('id');
+
+  if (localStorage.getItem('ratio'))
+    ratio = localStorage.getItem('ratio');
+  else
+    ratio = +document.querySelector('.calculating__choose_big .calculating__choose-item_active').getAttribute('data-ratio');
+
+  if (localStorage.getItem('height')) {
+    height = +localStorage.getItem('height');
+    document.querySelector('#height').value = height;
+  }
+
+  if (localStorage.getItem('weight')) {
+    weight = +localStorage.getItem('weight');
+    document.querySelector('#weight').value = weight;
+  }
+
+  if (localStorage.getItem('age')) {
+    age = +localStorage.getItem('age');
+    document.querySelector('#age').value = age;
+  }
+
+
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(`${selector} div`);
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+      if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+        elem.classList.add(activeClass);
+      }
+      if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+  initLocalSettings('.calculating__choose_big', 'calculating__choose-item_active');
+  initLocalSettings('#gender', 'calculating__choose-item_active');
 
   function calcTotal() {
-    let result = 0
+    let result = 0;
     if (!sex || !height || !weight || !age || !ratio) {
       calories.textContent = result;
       return;
@@ -387,14 +426,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     calories.textContent = Math.round(result * 100) / 100;
   }
+  calcTotal();
 
   function getStaticInformation(parentSelector, activeClass) {
     const elements = document.querySelectorAll(`${parentSelector} div`);
     elements.forEach(elem => elem.addEventListener('click', (e) => {
       if (e.target.getAttribute('data-ratio')) {
-        ratio = +e.target.getAttribute('data-ratio')
+        ratio = +e.target.getAttribute('data-ratio');
+        localStorage.setItem('ratio', ratio);
       } else {
         sex = e.target.getAttribute('id');
+        localStorage.setItem('sex', sex);
       }
       elements.forEach(el => {
         el.classList.remove(activeClass);
@@ -404,25 +446,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }));
   }
 
-
-
-  function getDynamicInformation(...selector) {
+  function getDynamicInformation(selector) {
     const input = document.querySelector(selector);
     input.addEventListener('input', () => {
+      if (input.value.trim().match(/\D/g)) {
+        input.classList.add('calculating__choose-item_error');
+        return;
+      } else {
+        input.classList.remove('calculating__choose-item_error');
+      }
       switch (input.getAttribute('id')) {
         case 'height':
           height = +input.value;
+          localStorage.setItem('height', height);
           break;
         case 'weight':
           weight = +input.value;
+          localStorage.setItem('weight', weight);
           break;
         case 'age':
           age = +input.value;
+          localStorage.setItem('age', age);
           break;
       }
       calcTotal();
     });
-    calcTotal();
   }
 
   getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
