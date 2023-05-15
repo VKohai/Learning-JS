b0a915b548ede9405550e0c6177104b0ffb36bb9
Kohaikaa <cherry.designwork@gmail.com>
2023-05-15 11:54:52 +0300

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+'use strict'
+const path = require('path');
+
+module.exports = {
+    mode: 'developmnet',
+    entry: './js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    watch: true,
+    devtool: 'source-map',
+    module: {}
+};
